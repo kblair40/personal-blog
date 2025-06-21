61575e63aa120c5b7f6b4f75af6fc5e5ea61bf38
Kevin Blair <kblair40@gmail.com>
2025-06-21 07:22:46 -0500

    Finish bulk of Arrow Diagram component

--- app/components/mdx/arrow-diagram.tsx
+++ app/components/mdx/arrow-diagram.tsx
@@ -1,20 +1,57 @@
 import React from "react";
+import clsx from "clsx";
+import { ArrowDown } from "lucide-react";
+import type { LucideProps } from "lucide-react";
 
 type Props = {
   blocks: (string | React.ReactNode)[];
+  wrapperClassName?: string;
+  blockClassName?: string;
+  blockWrapperClassName?: string;
+  iconProps?: LucideProps;
 };
 
-const ArrowDiagram = ({ blocks }: Props) => {
+const ArrowDiagram = ({
+  blocks,
+  wrapperClassName = "",
+  blockWrapperClassName = "",
+  blockClassName = "",
+  iconProps = {},
+}: Props) => {
+  const { className: iconClassName, ...rest } = iconProps;
+  const arrows: React.ReactNode[] = Array(blocks.length - 1).fill(
+    <ArrowDown size={20} className={clsx("mt-2", iconClassName)} {...rest} />
+  );
+
   return (
-    <div>
+    <div
+      className={clsx(
+        "flex flex-col items-center w-full space-y-2",
+        wrapperClassName
+      )}
+    >
       {blocks.map((block, i) => {
-        if (typeof block === "string") {
-          return (
-            <div className="flex items-center justify-center">{block}</div>
-          );
-        } else {
-          return block;
-        }
+        return (
+          <div
+            className={clsx(
+              "flex flex-col items-center w-full",
+              blockWrapperClassName
+            )}
+          >
+            <div
+              key={i}
+              className={clsx(
+                "w-full border flex items-center justify-center",
+                "py-1 px-4 rounded-sm",
+                blockClassName
+              )}
+            >
+              {block}
+            </div>
+
+            {arrows[i] && arrows[i]}
+          </div>
+        );
       })}
     </div>
   );
