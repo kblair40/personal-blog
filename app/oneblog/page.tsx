520558f6d338f3052559cf5de9c103a055ddc9d9
Kevin Blair <kblair40@gmail.com>
2025-06-28 07:13:57 -0500

    ADd example of feed fetch

--- app/oneblog/page.tsx
+++ app/oneblog/page.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Parser from "rss-parser";
 
 type Props = {};
 
-const OneBlog = (props: Props) => {
-  return <div>OneBlog</div>;
+const parser = new Parser();
+
+const OneBlog = async (props: Props) => {
+  const feed = await parser.parseURL("http://localhost:3001/rss");
+  console.log("\nFeed:", feed, "\n");
+  const comeau = await parser.parseURL("https://www.joshwcomeau.com/rss.xml");
+  console.log("\nComeau:", comeau, "\n");
+
+  return (
+    <div className="border absolute left-4 right-4 top-20">
+      <pre>{JSON.stringify({ feed, comeau }, null, 2)}</pre>
+    </div>
+  );
 };
 
-export default OneBlog
+export default OneBlog;
