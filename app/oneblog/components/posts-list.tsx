18ad5dbd8e0c073010febbab90190d475309aeae
Kevin Blair <kblair40@gmail.com>
2025-06-29 07:28:12 -0500

    Implement posts list, cleanup post code in page

---
+++ app/oneblog/components/posts-list.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { use } from "react";
+import Parser from "rss-parser";
+// import type { Item } from "rss-parser";
+
+import Post from "./post";
+
+type Posts = ({
+  [key: string]: any;
+} & Parser.Output<{
+  [key: string]: any;
+}>)[];
+
+type Props = {
+  //   posts: Item[][];
+  posts: Promise<Posts>;
+};
+
+const PostsList = ({ posts }: Props) => {
+  const postArrays = use(posts);
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      {postArrays.map((arr, i) => {
+        return arr.items.map((post, j) => {
+          return <Post key={`${i}-${j}`} post={post} />;
+        });
+      })}
+    </div>
+  );
+};
+
+export default PostsList;
--- app/oneblog/page.tsx
+++ app/oneblog/page.tsx
@@ -1,34 +1,20 @@
 import React from "react";
 import Parser from "rss-parser";
 
-import Post from "./components/post";
-
-type Props = {};
+import PostsList from "./components/posts-list";
 
 const parser = new Parser();
 
-const OneBlog = async (props: Props) => {
-  //   const feed = await parser.parseURL("http://localhost:3001/rss");
-  //   console.log("\nFeed:", feed.items[0], "\n");
-
-  //   const comeau = await parser.parseURL("https://www.joshwcomeau.com/rss.xml");
-  //   console.log("\nComeau:", comeau.items[0], "\n");
-
-  const postArrays = await Promise.all([
+const OneBlog = async () => {
+  const postArrays = Promise.all([
     parser.parseURL("http://localhost:3001/rss"),
     parser.parseURL("https://www.joshwcomeau.com/rss.xml"),
   ]);
   console.log(0, postArrays[0]);
 
   return (
-    <div className="flex flex-col gap-y-4">
-      {/* <pre>{JSON.stringify({ feed, comeau }, null, 2)}</pre> */}
-
-      {postArrays.map((arr, i) => {
-        return arr.items.map((post, j) => {
-          return <Post key={`${i}-${j}`} post={post} />;
-        });
-      })}
+    <div>
+      <PostsList posts={postArrays} />
     </div>
   );
 };
