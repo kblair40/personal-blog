eb949bad26cb3b344f4ee521f8500ad733215e1b
Kevin Blair <kblair40@gmail.com>
2025-07-01 08:09:42 -0500

    Add extra blog subscription options

--- app/oneblog/utils/blogs.ts
+++ app/oneblog/utils/blogs.ts
@@ -2,19 +2,43 @@ export const blogData = {
   "Kevin Blair": {
     rss: "http://localhost:3001/rss",
     meta: {
+      blogHome: "http://localhost:3001",
       creator: "Kevin Blair",
     },
   },
   OlegWock: {
     rss: "https://sinja.io/rss",
     meta: {
+      blogHome: "https://sinja.io/",
       creator: "Oleh Wock",
     },
   },
   "Josh W. Comeau": {
     rss: "https://www.joshwcomeau.com/rss.xml",
     meta: {
+      blogHome: "https://www.joshwcomeau.com",
       creator: "Josh W. Comeau",
+    },
+  },
+  LocalGhost: {
+    rss: "https://localghost.dev/articles.xml",
+    meta: {
+      blogHome: "https://localghost.dev",
+      creator: "Sophie Koonin",
+    },
+  },
+  HeydonWorks: {
+    rss: "https://heydonworks.com/feed.xml",
+    meta: {
+      blogHome: "https://heydonworks.com/",
+      creator: "HeydonWorks",
+    },
+  },
+  "News From Chris": {
+    rss: "https://cri.dev/rss.xml",
+    meta: {
+      blogHome: "https://cri.dev/",
+      creator: "Christian Fei",
     },
   },
 };
