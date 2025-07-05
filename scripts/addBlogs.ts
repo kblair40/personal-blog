e84c969d0ccacfbc5af952ae060620619c8304dd
Kevin Blair <kblair40@gmail.com>
2025-07-05 07:18:38 -0500

    Add proper script for addBlogs

--- scripts/addBlogs.ts
+++ scripts/addBlogs.ts
@@ -2,17 +2,42 @@ import db from "@/lib/db";
 import { blogData } from "@/oneblog/utils/blogs";
 import { blogsTable } from "@/lib/db/schema";
 
+async function updateBlogs() {
+    // Send blogs that were prevented from updating to 'unique' constraint here
+}
+
 async function addBlogs() {
-  for (const blogTitle in blogData) {
-    const { rss, blogHome, creator } = blogData[blogTitle];
+  for (const name in blogData) {
+    const { rss: rssUrl, blogHome: blogUrl, creator } = blogData[name];
 
-    const insertedBlog = await db.insert(blogsTable).values({
-      creator,
-      name: blogTitle,
-      blogUrl: blogHome,
-      rssUrl: rss,
-    });
+    const blog = { creator, name, blogUrl, rssUrl };
 
-    console.log("\nInserted Blog:", insertedBlog, "\n");
+    try {
+      const insertedBlog = await db.insert(blogsTable).values(blog).returning();
+
+      console.log("\nInserted Blog:", insertedBlog, "\n");
+    } catch (e) {
+      console.log("\nInsert failed:", e, { keys: Object.keys(e || {}) });
+      // @ts-ignore
+      const cause = e.cause as Record<string, any>;
+      console.log("\nCAUSE:", {
+        detail: cause.detail,
+        name: cause.name,
+        code: cause.code,
+        routine: cause.routine,
+        constraint: cause.constraint,
+      });
+
+      //   if ("cause" in (e as Record<string, any>)) {
+        if (e instanceof Error) {
+          // @ts-ignore
+        if (e.cause.constraint === "blogs_name_unique") {
+          console.log("ALREADY INSERTED:", JSON.stringify(blog, null, 2));
+        }
+      }
+    }
   }
 }
+
+
+addBlogs();
