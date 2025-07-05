2e4eb6939cbe6578fceb028d179e7e5cfe83d7de
Kevin Blair <kblair40@gmail.com>
2025-07-05 07:33:19 -0500

    Add log handler

--- scripts/addBlogs.ts
+++ scripts/addBlogs.ts
@@ -2,42 +2,43 @@ import db from "@/lib/db";
 import { blogData } from "@/oneblog/utils/blogs";
 import { blogsTable } from "@/lib/db/schema";
 
-async function updateBlogs() {
-    // Send blogs that were prevented from updating to 'unique' constraint here
+type Blog = {
+  id?: number;
+  creator: string;
+  name: string;
+  blogUrl: string;
+  rssUrl: string;
+};
+
+async function updateBlog(blog: Blog) {
+  // Send blogs that were prevented from updating to 'unique' constraint here
 }
 
 async function addBlogs() {
+  const blogs = await db.select().from(blogsTable);
+  console.log('\nAll blogs:', blogs, '\n')
+  return;
   for (const name in blogData) {
     const { rss: rssUrl, blogHome: blogUrl, creator } = blogData[name];
 
-    const blog = { creator, name, blogUrl, rssUrl };
+    const blog: Blog = { creator, name, blogUrl, rssUrl };
 
     try {
       const insertedBlog = await db.insert(blogsTable).values(blog).returning();
-
       console.log("\nInserted Blog:", insertedBlog, "\n");
     } catch (e) {
-      console.log("\nInsert failed:", e, { keys: Object.keys(e || {}) });
-      // @ts-ignore
-      const cause = e.cause as Record<string, any>;
-      console.log("\nCAUSE:", {
-        detail: cause.detail,
-        name: cause.name,
-        code: cause.code,
-        routine: cause.routine,
-        constraint: cause.constraint,
-      });
-
-      //   if ("cause" in (e as Record<string, any>)) {
-        if (e instanceof Error) {
-          // @ts-ignore
-        if (e.cause.constraint === "blogs_name_unique") {
-          console.log("ALREADY INSERTED:", JSON.stringify(blog, null, 2));
-        }
-      }
+      logError(e, blog);
     }
   }
 }
 
+function logError(e: unknown, blog: Blog) {
+  // @ts-ignore
+  if (e instanceof Error && e.cause?.constraint === "blogs_name_unique") {
+    console.log("ALREADY INSERTED:", JSON.stringify(blog, null, 2));
+  } else {
+    console.log("\nInsert failed (UNHANDLED):", e, "\n");
+  }
+}
 
 addBlogs();
