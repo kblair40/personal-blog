bc0a1036d16a8a5a3d3e9bfc3c455ac6ccb8c3cd
Kevin Blair <kblair40@gmail.com>
2025-07-05 07:50:10 -0500

    finish update and logError methods

--- scripts/addBlogs.ts
+++ scripts/addBlogs.ts
@@ -1,3 +1,5 @@
+import { eq } from "drizzle-orm";
+
 import db from "@/lib/db";
 import { blogData } from "@/oneblog/utils/blogs";
 import { blogsTable } from "@/lib/db/schema";
@@ -10,34 +12,59 @@ type Blog = {
   rssUrl: string;
 };
 
-async function updateBlog(blog: Blog) {
+async function updateBlog(blog: Blog): Promise<Blog | undefined> {
   // Send blogs that were prevented from updating to 'unique' constraint here
+  try {
+    const updatedBlog: Blog[] = await db
+      .update(blogsTable)
+      .set(blog)
+      .where(eq(blogsTable.name, blog.name))
+      .returning();
+
+    return updatedBlog[0];
+  } catch (e) {
+    logError(e, blog, "update");
+  }
+}
+
+function makeBlogMap(blogs: Blog[]): Record<string, Blog> {
+  return blogs.reduce((acc: Record<string, Blog>, blog) => {
+    acc[blog.name] = blog;
+    return acc;
+  }, {});
 }
 
 async function addBlogs() {
-  const blogs = await db.select().from(blogsTable);
-  console.log('\nAll blogs:', blogs, '\n')
-  return;
+  const blogs: Blog[] = await db.select().from(blogsTable);
+  const blogMap = makeBlogMap(blogs);
+
+  console.log("\nAll blogs:", blogs, "\n");
   for (const name in blogData) {
     const { rss: rssUrl, blogHome: blogUrl, creator } = blogData[name];
 
     const blog: Blog = { creator, name, blogUrl, rssUrl };
 
+    if (name in blogMap) {
+      const updatedBlog = await updateBlog(blog);
+      console.log("\nUpdated Blog:", updatedBlog);
+      continue;
+    }
+
     try {
       const insertedBlog = await db.insert(blogsTable).values(blog).returning();
-      console.log("\nInserted Blog:", insertedBlog, "\n");
+      console.log("\nInserted Blog:", insertedBlog);
     } catch (e) {
-      logError(e, blog);
+      logError(e, blog, "insert");
     }
   }
 }
 
-function logError(e: unknown, blog: Blog) {
+function logError(e: unknown, blog: Blog, type: "insert" | "update") {
   // @ts-ignore
-  if (e instanceof Error && e.cause?.constraint === "blogs_name_unique") {
+  if (e instanceof Error && e.cause?.constraint?.endsWith("unique")) {
     console.log("ALREADY INSERTED:", JSON.stringify(blog, null, 2));
   } else {
-    console.log("\nInsert failed (UNHANDLED):", e, "\n");
+    console.log(`\n${type.toUpperCase()} failed (UNHANDLED):`, e, "\n");
   }
 }
 
