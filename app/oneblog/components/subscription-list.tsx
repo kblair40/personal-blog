4b865a5829538be4c02e19592ba917f7200423c1
Kevin Blair <kblair40@gmail.com>
2025-07-06 05:25:59 -0500

    Refactor subscription list using fetched blogs

--- app/oneblog/components/subscription-list.tsx
+++ app/oneblog/components/subscription-list.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, use } from "react";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
-import { blogData } from "../utils/blogs";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import type { Blog } from "@/lib/db/schema.types";
 
 type Props = {
   userId: number;
+  blogs: Promise<Blog[]>;
 };
 
-const SubscriptionList = ({ userId }: Props) => {
+const SubscriptionList = ({ userId, blogs: _blogs }: Props) => {
+  const blogs = use(_blogs);
+
   const [loading, setLoading] = useState(false);
 
   async function toggleSubscription() {
@@ -18,8 +23,23 @@ const SubscriptionList = ({ userId }: Props) => {
   }
 
   return (
-    <div className="flex flex-col gap-y-1">
-      {Object.entries(blogData).map(([title, meta], i) => {
+    <div className="flex flex-col gap-y-2">
+      {blogs.map(({ id, name, blogUrl, creator }) => {
+        return (
+          <div key={id} className="flex items-center">
+            <Checkbox name={name} id={name} />
+            <Label className="ml-2" htmlFor={name}>
+              {name} - {creator || name}
+            </Label>
+
+            <a className="ml-4" href={blogUrl} target="_blank">
+              <ExternalLink size={18} />
+            </a>
+          </div>
+        );
+      })}
+
+      {/* {Object.entries(blogData).map(([title, meta], i) => {
         return (
           <div key={i} className="flex items-center gap-x-2">
             <Checkbox name={title} id={title} />
@@ -28,7 +48,7 @@ const SubscriptionList = ({ userId }: Props) => {
             </Label>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
--- app/oneblog/subscriptions/page.tsx
+++ app/oneblog/subscriptions/page.tsx
@@ -3,11 +3,18 @@ import React from "react";
 import type { Session } from "@/lib/types";
 import { getSession } from "@/lib/session";
 import SubscriptionList from "../components/subscription-list";
+import db from "@/lib/db";
+import { blogsTable } from "@/lib/db/schema";
+import type { Blog } from "@/lib/db/schema.types";
 
 type Props = {};
 
 const OneBlogSubscriptions = async (props: Props) => {
+  // const blogRes = fetch("localhost:3001/api/blogs");
   const session = await getSession();
+
+  const blogs: Promise<Blog[]> = db.select().from(blogsTable);
+  // const subscriptions =
 
   if (!session) {
     return <div>You are not logged in</div>;
@@ -15,7 +22,7 @@ const OneBlogSubscriptions = async (props: Props) => {
 
   return (
     <div>
-      <SubscriptionList userId={session.id} />
+      <SubscriptionList userId={session.id} blogs={blogs} />
     </div>
   );
 };
