d815b6b3304343f15d3ddb698849b4d4e6f84167
Kevin Blair <kblair40@gmail.com>
2025-07-19 10:35:13 -0400

    Style improvements

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,11 @@
 export default function NotFound() {
   return (
-    <section>
+    <section className="centered-col h-fillpage pb-20">
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+
+      <p>The page you are looking for does not exist.</p>
     </section>
-  )
+  );
 }
