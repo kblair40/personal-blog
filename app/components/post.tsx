402d88a482cee4f30c04aaf40b91e594335da906
Kevin Blair <kblair40@gmail.com>
2025-07-19 10:15:50 -0500

    Style improvements

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -7,13 +7,18 @@ type Props = {
 
 const Post = ({ post }: Props) => {
   return (
-    <div className="w-fit">
-      <a target="_blank" href={post.link}>
+    <div className="w-fit group">
+      <a
+        target="_blank"
+        href={post.link}
+        className="underline-offset-2 hover:underline"
+      >
         <h3 className="font-semibold text-lg">{post.title}</h3>
       </a>
+
       <div className="flex gap-x-3">
-        <p className="opacity-20">{post.creator || "?"}</p>
-        <p className="opacity-20">
+        <p className="opacity-70">{post.creator || "?"}</p>
+        <p className="opacity-70">
           {post.isoDate ? new Date(post.isoDate).toLocaleDateString() : "?"}
         </p>
       </div>
