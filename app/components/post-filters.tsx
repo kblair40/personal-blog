699f1f3431ecff053e8e5e838a47577797d17ece
Kevin Blair <kblair40@gmail.com>
2025-07-21 06:00:47 -0500

    Remove useState import

--- app/components/post-filters.tsx
+++ app/components/post-filters.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { CalendarArrowDown, CalendarArrowUp, ChevronDown } from "lucide-react";
 import {
   useQueryState,
