ab2b439acd2421a0fd0ae56bf1e3d8cea4611d64
Kevin Blair <kblair40@gmail.com>
2025-07-26 07:26:59 -0400

    Add skeleton for AddBlog form

--- app/components/subscription-request-modal.tsx
+++ app/components/subscription-request-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, type FormEvent } from "react";
 import clsx from "clsx";
 
 import {
@@ -14,6 +14,9 @@ import {
 import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "./ui/textarea";
+
+type FormInputElement = HTMLInputElement | HTMLTextAreaElement;
 
 type Props = {};
 
@@ -21,9 +24,11 @@ const SubscriptionRequestSheet = (props: Props) => {
   const [formData, setFormData] = useState({
     homeUrl: "",
     rssUrl: "",
+    details: "",
   });
+  const [saving, setSaving] = useState(false);
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<FormInputElement>) {
     const {
       target: { name, value },
     } = e;
@@ -32,6 +37,16 @@ const SubscriptionRequestSheet = (props: Props) => {
     setFormData((cur) => {
       return { ...cur, [name]: value };
     });
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    console.log("submit");
+    setSaving(true);
+
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    //
   }
 
   return (
@@ -48,35 +63,59 @@ const SubscriptionRequestSheet = (props: Props) => {
           <SheetTitle>Request a Blog</SheetTitle>
         </SheetHeader>
 
-        <div className="px-4">
-          <section className="grid grid-rows-2 gap-y-4 md:grid-cols-2 md:grid-rows-1 md:gap-y-0 md:gap-x-8">
-            <div className="w-full">
-              <Label className="mb-1" htmlFor="homeUrl">
-                Homepage URL
-              </Label>
-              <Input
-                id="homeUrl"
-                name="homeUrl"
-                placeholder="www.blogilike.com"
-                value={formData.homeUrl}
-                onChange={handleChange}
-              />
-            </div>
+        <form
+          onSubmit={handleSubmit}
+          // className="page-wrapper max-w-xs sm:max-w-3xl mx-auto"
+        >
+          <div className="px-4 flex flex-col gap-y-4">
+            <section className="grid grid-rows-2 gap-y-4 md:grid-cols-2 md:grid-rows-1 md:gap-y-0 md:gap-x-8">
+              <div className="w-full">
+                <Label className="mb-1" htmlFor="homeUrl">
+                  Homepage URL
+                </Label>
+                <Input
+                  id="homeUrl"
+                  name="homeUrl"
+                  placeholder="www.blogilike.com"
+                  value={formData.homeUrl}
+                  onChange={handleChange}
+                />
+              </div>
 
-            <div className="w-full">
-              <Label className="mb-1" htmlFor="rssUrl">
-                RSS Feed URL
+              <div className="w-full">
+                <Label className="mb-1" htmlFor="rssUrl">
+                  RSS Feed URL
+                </Label>
+                <Input
+                  id="rssUrl"
+                  name="rssUrl"
+                  placeholder="www.blogilike.com/rss.xml"
+                  value={formData.rssUrl}
+                  onChange={handleChange}
+                />
+              </div>
+            </section>
+
+            <section>
+              <Label className="mb-1" htmlFor="details">
+                Anything we should know?
               </Label>
-              <Input
-                id="rssUrl"
-                name="rssUrl"
-                placeholder="www.blogilike.com/rss.xml"
-                value={formData.rssUrl}
+              <Textarea
+                id="details"
+                name="details"
+                value={formData.details}
                 onChange={handleChange}
               />
-            </div>
-          </section>
-        </div>
+            </section>
+          </div>
+
+          <div className="flex items-center justify-end mt-6 gap-x-4">
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
       </SheetContent>
     </Sheet>
   );
