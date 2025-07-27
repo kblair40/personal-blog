9603a20ad2693c551539da515168298a57061ec2
Kevin Blair <kblair40@gmail.com>
2025-07-27 09:54:22 -0400

    Add proper url types for update and insert schema

--- app/lib/db/schema.types.ts
+++ app/lib/db/schema.types.ts
@@ -37,23 +37,16 @@ export type SubscriptionUpdate = z.infer<typeof subscriptionUpdateSchema>;
 export const blogRequestSelectSchema = createSelectSchema(
   schema.blogRequestsTable
 );
-// export const blogRequestInsertSchema = createInsertSchema(
-//   // Todo: Add validation for url fields with zod.url
-//   schema.blogRequestsTable
-// );
 export const blogRequestInsertSchema = createInsertSchema(
-  // Todo: Add validation for url fields with zod.url
   schema.blogRequestsTable
 )
   .omit({ blogUrl: true, rssUrl: true })
-  .extend({
-    blogUrl: z.url(),
-    rssUrl: z.url(),
-  });
+  .extend({ blogUrl: z.url(), rssUrl: z.url() });
 export const blogRequestUpdateSchema = createUpdateSchema(
-  // Todo: Add validation for url fields with zod.url
   schema.blogRequestsTable
-);
+)
+  .omit({ blogUrl: true, rssUrl: true })
+  .extend({ blogUrl: z.url(), rssUrl: z.url() });
 export type BlogRequest = z.infer<typeof blogRequestSelectSchema>;
 export type BlogRequestInsert = z.infer<typeof blogRequestInsertSchema>;
 export type BlogRequestUpdate = z.infer<typeof blogRequestUpdateSchema>;
